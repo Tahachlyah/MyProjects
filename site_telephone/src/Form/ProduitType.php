1a69b6470e5ddbac6424b855496765c3171f6dcd
tahachlyah <tahachlyah@gmail.com>
2023-05-25 22:30:30 +0200

    ajouter les fichiers de mon projet

---
+++ site_telephone/src/Form/ProduitType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use App\Entity\Category;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('prix')
+            ->add('quantity')
+            ->add('ref')
+            ->add('image', FileType::class,[
+                'label' => 'charger une image',
+                'data_class' => null,
+                'required' => false
+            ])
+            ->add('category', EntityType::class,[
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
